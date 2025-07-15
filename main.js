4b3ca83fc0f1ae2ed0375902c105c9f7fe79dd0b
borisrosedev <boris.rose.dev@gmail.com>
2025-07-15 19:51:41 +0200

    feat/login

---
+++ main.js
@@ -0,0 +1,45 @@
+
+
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+
+
+class Phishing {
+    emailId= null;
+    passwordId = null; 
+    constructor(){
+
+        this.email = document.getElementById('email')
+        this.password = document.getElementById('password')
+        this.email.addEventListener('input', (event) => this.inputHandler(event, "email"))
+        this.password.addEventListener('input', (event) => this.inputHandler(event, "password"))
+    }
+
+    inputHandler(event, name){
+
+        if (this.emailId && name == "email"){
+            clearTimeout(this.emailId)
+        }
+
+        if(this.passwordId && name == "password"){
+            clearTimeout(this.passwordId)
+        }
+    
+        if(name == "email"){
+                this.emailId = setTimeout(() => {
+                    console.log(`${name}`,event.target.value)
+                }, 5000)
+        } else {
+            this.passwordId = setTimeout(() => {
+                    console.log(`${name}`,event.target.value)
+            }, 5000)
+        }
+      
+   
+    }
+
+
+
+}
+
+const phishing = new Phishing()
